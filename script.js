26163d7fff41b5987b7e3539244fe74ec9ea4f32
vikashdeveloper13 <codebyverma543@gmail.com>
2024-08-28 09:00:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+gsap.from("#container #inner-box .krishna",{
+    y:-300,
+    duration:2,
+    delay:1,
+})
+gsap.from("#container #inner-box .girl",{
+    y:600,
+    duration:4,
+    delay:1.5,
+})
+gsap.from("#main #con h1",{
+    x:-600,
+    opacity:0,
+    duration:4,
+    delay:1,
+})
+gsap.from("#container h3",{
+    x:600,
+    opacity:0,
+    duration:4,
+    delay:1,
+})
+gsap.from("#container #inner-box .matki",{
+    y:200,
+    delay:1,
+    duration:2,
+})
+gsap.from("#container #inner-box .matki2",{
+    y:200,
+    delay:1,
+    duration:2,
+})
